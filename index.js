262b0c13faba90e8d4335e0bf8c572a39987c04b
rahulxo19 <rahulchhikara2001@gmail.com>
2023-03-08 15:41:08 +0530

    3rd commit

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const http = require('http');
-const routes = require('./routes');
+// const routes = require('./routes');
 
-const server = http.createServer(routes);
+const express = require('express');
+const app = express();
 
-server.listen(4000, () => {
-  console.log('Server running on port 4000');
+const server = http.createServer(app);
+
+app.use((req, res, next) => {
+  next();
+
+})
+app.use((req, res, next) => {
+  res.send('<h2>Hello from 2nd middleware</h2>')
+})
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
