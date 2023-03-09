7b8cf202e78b22359b46109f92e8e67bf85649ef
rahulxo19 <rahulchhikara2001@gmail.com>
2023-03-09 13:42:03 +0530

    4th commit

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const http = require('http');
-// const routes = require('./routes');
-
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req, res, next) => {
-  next();
-
-})
-app.use((req, res, next) => {
-  res.send('<h2>Hello from 2nd middleware</h2>')
+app.use('/add-product', (req,res,next) => {
+  res.send('<form action="/" method="POST"><input type="text" name="title"><input type="number" name="size"><button type=submit>Submit</button></form>')
 })
 
-server.listen(3000, () => {
+app.use('/', (req,res,next) => {
+
+  if(req.method === "POST"){
+    console.log(req.body);
+  res.send('<h1>Product has been added</h1>');
+  }
+})
+
+app.listen(3000, () => {
   console.log('Server running on port 3000');
 });
