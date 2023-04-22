ae032e6a8e8d09174e92cb53b89d92145f3accf5
rahulxo19 <rahulchhikara2001@gmail.com>
2023-04-22 14:25:37 +0530

    Forgot Password & Integrating Mailing Service

--- backend/test.js
+++ backend/test.js
@@ -1,6 +1,3 @@
 const crypto = require('crypto');
 
-// Generate a random secret key with a length of 64 bytes (512 bits)
-const secretKey = crypto.randomBytes(10).toString('hex');
-
-console.log('JWT Secret Key:', secretKey);
+reciever[0].email = "hello@gmail.com";
