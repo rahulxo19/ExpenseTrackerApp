344e893540b4e71b7616bb5457f9e8ca5f2ad8ac
rahulxo19 <rahulchhikara2001@gmail.com>
2023-04-25 16:47:30 +0530

    Lets go live

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,50 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const fs = require("fs");
+require("dotenv").config();
 
-const sequelize = require('./util/database')
-const form = require('./routes/form')
-const User = require('./model/signup')
-const Expenses = require('./model/form')
-const Order = require('./model/orders')
-const forgotpassword = require('./model/forgotpassword')
+const sequelize = require("./util/database");
+const form = require("./routes/form");
+const User = require("./model/signup");
+const Expenses = require("./model/form");
+const Order = require("./model/orders");
+const forgotpassword = require("./model/forgotpassword");
 
-User.hasMany(Order)
+User.hasMany(Order);
 Order.belongsTo(User);
 
-User.hasMany(Expenses)
+User.hasMany(Expenses);
 Expenses.belongsTo(User);
 
-User.hasMany(forgotpassword)
+User.hasMany(forgotpassword);
 forgotpassword.belongsTo(User);
 
 const app = express();
 
-app.use(cors());
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan("combined", { stream : accessLogStream }));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', form)
+app.use("/", form);
 
 // app.use('/', form)
 
-sequelize.sync().then(() => {
-  app.listen(3000);
-}).catch(err => console.log(err))
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
